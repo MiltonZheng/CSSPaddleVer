fb5a35dc756b319d937bfb03cf1f502ecdbba946
MiltonZheng <zheng12238@gmail.com>
2023-08-18 14:54:15 +0800

    解决特征提取爆内存问题

--- hashLabelGen.py
+++ hashLabelGen.py
@@ -1,9 +1,9 @@
-import utils
 import os
 import h5py
 import paddle
-from matplotlib import pyplot as plt
 from dataset import MyDataset
+from tqdm import tqdm
+import numpy as np
 
 
 __PATH__ = "../datasets/NWPU-RESISC45/train"
@@ -11,29 +11,33 @@ file = os.path.join(__PATH__, "data.hy")
 try:
     h5data = h5py.File(file, 'r')
 except:
-    raise IOError('Dataset not found. Please make sure the dataset was downloaded.')
+    raise IOError('Dataset not found. Please make sure the dataset is stored in the right directory.')
 
-train_image = h5data['imgae'][()]
+train_image = h5data['image'][()]
 train_label = h5data['label'][()]
 train_set = MyDataset(train_image, train_label)
 
-# 定义并初始化数据读取器
-train_loader = paddle.io.DataLoader(train_set, batch_size=64, shuffle=True, num_workers=1, drop_last=True)
+# define & initialize the data loader
+train_loader = paddle.io.DataLoader(train_set, batch_size=32, shuffle=True, num_workers=1, drop_last=False)
 
-# 调用 DataLoader 迭代读取数据
-for batch_id, data in enumerate(train_loader()):
+vgg16 = paddle.vision.models.vgg16(pretrained=True)
+features = []
+# call the DataLoader to read data iteratively
+for batch_id, data in enumerate(tqdm(train_loader(), desc="extracting feature")):
     images, labels = data
-    print("batch_id: {}, 训练数据shape: {}, 标签数据shape: {}".format(batch_id, images.shape, labels))
-    break
+    # *The model requires the input dimension to be [3, height, width]
+    # *but imread reads images as [width, height, 3], so the data needs to be transposed
+    images = paddle.transpose(images.astype('float32'), perm=[0,3,1,2])
+    feature = vgg16(images)
+    # !remember to move the extracted features from gpu memory to main memory or gpu memory will run out
+    feature = paddle.Tensor(feature).detach().cpu().numpy()
+    features.extend(feature)
 
-print(h5data['label'][()][0])
-img = h5data['image'][()][0]
-plt.imshow(img)
-plt.show()
+# store the features as hy file
+features = np.asarray(features)
+print("feature sieze:{}".format(features.shape))
+featuresh5py = h5py.File(os.path.join(__PATH__, 'features.hy'), 'w')
+featuresh5py.create_dataset("features", data=features)
+featuresh5py.close()
 
 
-# # 调用内置vgg16模型
-# vgg16 = paddle.vision.models.vgg16(pretrained=True)
-# input = paddle.rand([5, 3, 224, 224])
-# output = vgg16(input)
-# print(output.shape)
