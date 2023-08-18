0c95432332a6e049a9e2fd0ad71469115ce50812
MiltonZheng <zheng12238@gmail.com>
2023-08-18 17:27:28 +0800

    local剩余

--- hashLabelGen.py
+++ hashLabelGen.py
@@ -2,7 +2,7 @@
 Author: MiltonZheng zheng12238@gmail.com
 Date: 2023-08-18 10:25:31
 LastEditors: MiltonZheng zheng12238@gmail.com
-LastEditTime: 2023-08-18 15:53:19
+LastEditTime: 2023-08-18 16:33:27
 FilePath: \CSSPaddleVer\hashLabelGen.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -55,23 +55,31 @@ def extract_feature(data_loader, filepath):
     # store the features as hy file
     features = np.asarray(features)
     print("feature sieze:{}".format(features.shape))
-    featuresh5py = h5py.File(os.path.join(filepath, 'features.hy'), 'w')
-    featuresh5py.create_dataset("features", data=features)
-    featuresh5py.close()
+    features_h5 = h5py.File(os.path.join(filepath, 'features.hy'), 'w')
+    features_h5.create_dataset("features", data=features)
+    features_h5.close()
 
 def create_hashtags(filepath, bits=48, nei=10):
     featuresfile = h5py.File(os.path.join(filepath, 'features.hy'), 'r')
     features = featuresfile["features"][()]
     print("features size:{}".format(features.shape))
     print("decompose features...")
-    features_pca = manifold.SpectralEmbedding(n_components=bits, n_neighbors=nei).fit_transform(features)
+    # features_pca = manifold.SpectralEmbedding(n_components=bits, n_neighbors=nei).fit_transform(features)
+    features_pca = decomposition.PCA(n_components=bits).fit_transform(features)
     hashtags = features_pca >= 0
     hashtags = hashtags.astype(np.int32)
     print(hashtags.shape)
+    return hashtags
 
 if __name__ == "__main__":
     path = "../datasets/NWPU-RESISC45/train"
-    # data_loader = build_data_loader(filepath=path)
-    # extract_feature(data_loader, filepath=path)
-    create_hashtags(filepath=path)
+    data_loader = build_data_loader(filepath=path)
+    extract_feature(data_loader, filepath=path)
+    hashtags = create_hashtags(filepath=path)
+    print("storing hashtags...")
+    hashtags_h5 = h5py.File(os.path.join(path, 'hashtags.hy'), 'w')
+    hashtags_h5.create_dataset("hashtagss", data=hashtags)
+    hashtags_h5.close()
+    sum = np.sum(hashtags, axis=0)
+    print(sum)
     
